d9b6c0beb4e6651d4740d9f4a4d1a6508ff74a02
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-03-29 14:05:21 +0200

    Merge branch 'main' of https://github.com/antonsjolander/express

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(morgan("tiny"));
+
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
 	res.send("Hello World!");
+});
+
+app.get("/about", (req, res) => {
+	res.send(`<p style="color:red">HELLO</p>`);
+});
+
+app.post("/post", (req, res) => {
+	const { message } = req.body;
+	res.send(`here is your message: ${message}`);
 });
 
 app.listen(port, () => {
